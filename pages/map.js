a4590cc064cf3408ff2b81b57b21aaea562e69e5
dc <danielmcollins1996@gmail.com>
2020-09-29 00:10:11 -0600

    added setLocation

--- pages/map.js
+++ pages/map.js
@@ -1,11 +1,34 @@
 import GoogleMapReact from 'google-map-react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 const AnyReactComponent = ({text}) => <div>{text}</div>;
 
 export default function Map() {
-  const [location, setLocation] = useState({lat: 11.0168, lng: 76.9558 })
+  const [location, setLocation] = useState({lat: 65.0168, lng: -114.2558 })
   const [zoom, setZoom] = useState(11);
+  const [error, setError] = useState(null);
+
+  const onChange = ({coords}) => {
+    setLocation({
+      lat: coords.latitude,
+      lng: coords.longitude
+    })
+  }
+
+  const onError = (error) => {
+    setError(error.message)
+  }
+
+
+  useEffect(() => {
+    const geo = navigator.geolocation
+    if(!geo) {
+      return
+    }
+    let watcher = geo.watchPosition(onChange, onError);
+    return () => geo.clearWatch(watcher);
+  },[])
+
   return (
       <div className="p-4 shadow rounded bg-white">
         {/* <h1 className="text-purple-500 leading-normal">Next.js</h1>
